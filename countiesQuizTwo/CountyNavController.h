edd1b3c9f11acab857dd1c4e7491f5c44a66a07f
Sam Baguley <hello@sambaguley.com>
2012-03-08 18:51:53 +0000

    thursday evening

---
+++ countiesQuizTwo/CountyNavController.h
@@ -0,0 +1,13 @@
+//
+//  CountyNavController.h
+//  countiesQuizTwo
+//
+//  Created by Sam Baguley on 08/03/2012.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CountyNavController : UINavigationController
+
+@end
